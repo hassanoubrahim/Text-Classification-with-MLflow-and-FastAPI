4d4d367449c0be94db8c54ebe133b5a7b42f38cf
hassanoubrahim <65039956+hassanoubrahim@users.noreply.github.com>
2023-11-29 15:07:30 +0000

    Create app.py

--- app.py
+++ app.py
@@ -1,41 +1,38 @@
-from flask import Flask, render_template, abort, request
-import joblib
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+import uvicorn
+import joblib
 
-class ValidateData(BaseModel):
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+class ValidateDataExam(BaseModel):
     review:str
 
 
+@app.get('/exam')
+def home(request:Request):
+    data=None
+    return "welcome!"
 
-app = Flask(__name__)
+@app.get('/exam/predict')
+def predict(review:str):
+    data = {
+        'review':review
+    }
+    d = ValidateDataExam(**data)
+    filename = '/models/LR_model.pkl'
+    loaded_model = joblib.load(open(filename, 'rb'))
+    #transofrm
+    vectorizer = joblib.load(open('/models/fitted_vectorizer.pkl', 'rb'))
+    try:
+        model_input = vectorizer.transform([review])
+        res = loaded_model.predict(model_input)
+    except:
+        res = 1
+    return res
 
-import os
-# Get the directory path of the current script
-current_dir = os.path.dirname(__file__)
-
-
-@app.route('/project/<project_id>', methods=['POST', 'GET'])
-def project(project_id):
-    if project_id == '12':
-        if request.method == "POST":
-            data = request.form
-            d = ValidateData(**data)
-            filename = os.path.join(current_dir, 'projects/Mlflow_FastAPi_Flask/models/LR_model.pkl')
-            loaded_model = joblib.load(open(filename, 'rb'))
-            # transofrm
-            vec_file = os.path.join(current_dir, 'projects/Mlflow_FastAPi_Flask/models/fitted_vectorizer.pkl')
-            vectorizer = joblib.load(open(vec_file, 'rb'))
-            try:
-                model_input = vectorizer.transform(data['review'])
-                res = loaded_model.predict(model_input)
-            except:
-                res = 1
-            return  render_template('details/project-details.html', res=res)
-        elif request.method == "GET":
-            return render_template('details/project-details.html')
-
-@app.get('/experience')
-def experience():
-    return "coming soon <a href='../' > Go back </a>"
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
